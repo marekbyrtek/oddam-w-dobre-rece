c587c40e327e1f883de7cfee833a23621187e196
marekbyrtek <marek.byrtekk@gmail.com>
2021-10-06 12:45:59 +0200

    Dodana funkcjonalność react scroll

--- src/components/Home/HomeContact.js
+++ src/components/Home/HomeContact.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link, Element } from "react-scroll";
 
 const HomeContact = () => {
-    return <h1 style={{height: "100vh"}}>Skontaktuj się z nami</h1>
+    return (
+        <Element name="contact" style={{height: "100vh"}}>
+            <h1>Skontaktuj się z nami</h1>
+            <ul>
+                <li><Link to="header" smooth={true} duration={1000}>Nagłówek</Link></li>
+                <li><Link to="threeColumns" smooth={true} duration={1000}>Trzy kolumny</Link></li>
+                <li><Link to="fourSteps" smooth={true} duration={1000}>Wystarczą 4 proste kroki</Link></li>
+                <li><Link to="aboutUs" smooth={true} duration={1000}>O nas</Link></li>
+                <li><Link to="whoWeHelp" smooth={true} duration={1000}>Komu pomagamy</Link></li>
+                <li><Link to="contact" smooth={true} duration={1000}>Skontaktuj się z nami</Link></li>
+            </ul>
+        </Element>
+    )
 }
 
 export default HomeContact;
--- src/components/Home/HomeFourSteps.js
+++ src/components/Home/HomeFourSteps.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link, Element } from "react-scroll";
 
 const HomeFourSteps = () => {
-    return <h1 style={{height: "100vh"}}>Wystarczą 4 proste kroki</h1>
+    return (
+        <Element name="fourSteps" style={{height: "100vh"}}>
+            <h1>Wystarczą 4 proste kroki</h1>
+            <ul>
+                <li><Link to="header" smooth={true} duration={1000}>Nagłówek</Link></li>
+                <li><Link to="threeColumns" smooth={true} duration={1000}>Trzy kolumny</Link></li>
+                <li><Link to="fourSteps" smooth={true} duration={1000}>Wystarczą 4 proste kroki</Link></li>
+                <li><Link to="aboutUs" smooth={true} duration={1000}>O nas</Link></li>
+                <li><Link to="whoWeHelp" smooth={true} duration={1000}>Komu pomagamy</Link></li>
+                <li><Link to="contact" smooth={true} duration={1000}>Skontaktuj się z nami</Link></li>
+            </ul>
+        </Element>
+    )
 }
 
 export default HomeFourSteps;
