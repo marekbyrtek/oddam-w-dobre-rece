f0c82193301034218a658fbaaf515beb80eb96e1
marekbyrtek <marek.byrtekk@gmail.com>
2021-10-09 18:21:32 +0200

    Wysyłka formularza

--- src/components/Home/Contact/ContactForm.js
+++ src/components/Home/Contact/ContactForm.js
@@ -1,32 +1,62 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Form, Row, Col, Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 
 const ContactForm = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-    console.log(errors);
+    const { isDirty, register, handleSubmit, formState: { errors } } = useForm();
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const msgRef = useRef();
+
+    // const onSubmit = data => console.log(data);
+    // console.log(errors);
+
+    const onSubmit = data => {
+        console.log(data);
+        console.log(errors);
+        fetch("https://fer-api.coderslab.pl/v1/portfolio/contact", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+        }})
+        .then(resp => {
+            if (resp.ok) {
+                return resp.json();
+            } else {
+                throw new Error("Błąd sieci");
+            }
+        })
+        .catch(err => console.log(err))
+    }
 
     return (
         <Form onSubmit={handleSubmit(onSubmit)}>
             <Row className="mb-3">
                 <Form.Group as={Col} controlId="formGridEmail">
                     <Form.Label>Wpisz swoje imię</Form.Label>
-                    <Form.Control type="text" placeholder="Krzysztof" {...register("imię", {required: true, maxLength: 20, pattern: /[a-zA-Z]+/i})} />
+                    <Form.Control type="text" placeholder="Krzysztof" ref={nameRef} {...register("name", {required: true, maxLength: 20,  pattern: {
+                            value: /^[a-zA-Z]*$/,
+                            message: "invalid email address"
+                        }}
+                    )} />
+                <p>{errors?.name && "Złe imie"}</p>
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridPassword">
                     <Form.Label>Wpisz swój email</Form.Label>
-                    <Form.Control type="email" placeholder="abc@xyz.pl" {...register("email", {required: true, maxLength: 50})} />
+                    <Form.Control type="text" placeholder="abc@xyz.pl" ref={emailRef} {...register("email", {required: true, maxLength: 50})} />
                 </Form.Group>
             </Row>
 
             <Form.Group className="mb-3" controlId="formGridAddress1">
                 <Form.Label>Wpisz swoją wiadomość</Form.Label>
-                <Form.Control as="textarea" rows={5} placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel neque posuere, auctor lorem non, porta tortor. Quisque vehicula nisi et massa commodo porttitor. Mauris facilisis ullamcorper urna ut semper. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum arcu nunc, facilisis et nunc in, consequat convallis lorem." {...register("wiadomość", {required: true, minLength: 120})} />
+                <Form.Control as="textarea" rows={5} placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel neque posuere, auctor lorem non, porta tortor. Quisque vehicula nisi et massa commodo porttitor. Mauris facilisis ullamcorper urna ut semper. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum arcu nunc, facilisis et nunc in, consequat convallis lorem." ref={msgRef} {...register("message", {required: true, minLength: 120})} />
             </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
+            {console.log(errors)}
+            <p>{errors?.message?.type}</p>
+            <Button disabled={isDirty} variant="primary" type="submit">
+                Wyślij
             </Button>
         </Form>
     );
